c7fe9a924b96480e4b9334a136ae5d2827987b9f
gyorgymark <78681186+Kescefko@users.noreply.github.com>
2025-01-02 10:02:08 +0100

    Create azure_linux_vm_deploy.sh
    script to create azure vm linux for general purposes within development environment

---
+++ azure_linux_vm_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Prompt the user for the admin username
+read -p "Enter the admin username for the VM: " admin_username
+
+# Create the Azure VM with the entered username
+az vm create \
+    --name MyLinuxAgent \
+    --resource-group gr-gyorgy-tailspin-dev-weu \
+    --location westeurope \
+    --image canonical:0001-com-ubuntu-server-focal:20_04-lts:latest \
+    --size Standard_DS2_v2 \
+    --admin-username "$admin_username" \
+    --generate-ssh-keys
